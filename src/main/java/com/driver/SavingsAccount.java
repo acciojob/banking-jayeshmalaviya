aad12cb09d0c1f98e33609860d4aa6baaf350f5a
jayesh malaviya <jayesh24017@gmail.com>
2023-03-21 11:42:02 +0530

    second time

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -6,34 +6,37 @@ public class SavingsAccount extends BankAccount{
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
-        super(name,balance);
-        this.maxWithdrawalLimit=maxWithdrawalLimit;
-        this.rate=rate;
+        super(name,balance,0);
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+        this.rate= rate;
 
     }
     public void withdraw(double amount) throws Exception {
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
-        if(amount>this.maxWithdrawalLimit)throw new Exception("Maximum Withdraw Limit Exceed");
-        else if(amount>getBalance())throw  new Exception("Insufficient Balance");
-        else{
-            double x=getBalance()-amount;
-            setBalance(x);
-        }
-
+        if(amount<=maxWithdrawalLimit)
+            super.withdraw(amount);
+        else
+            throw new Exception("Maximum Withdraw Limit Exceeded");
     }
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
-        double SI= (getBalance()*this.rate*years)/100;
-        return SI;
+        return getBalance()*(1+(rate*years)/100);
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
-        double CI = getBalance() * (Math.pow((1 + this.rate / times), times*years));
-        return CI;
+        double compoundInterest = getBalance()*Math.pow((1+rate/(100*times)),(times*years));
+        return getBalance()*Math.pow((1+rate/(100*times)),(times*years));
     }
 
+    public double getRate() {
+        return rate;
+    }
+
+    public double getMaxWithdrawalLimit() {
+        return maxWithdrawalLimit;
+    }
 }
